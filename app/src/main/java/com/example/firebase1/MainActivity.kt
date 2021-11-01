7fed39218df2a1d6114c7d18f04db15240c1e141
zahraMajed <zahra_m.alzawad@hotmail.com>
2021-11-01 14:24:59 +0300

    save and get

--- app/src/main/java/com/example/firebase1/MainActivity.kt
+++ app/src/main/java/com/example/firebase1/MainActivity.kt
@@ -3,14 +3,30 @@ package com.example.firebase1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     var TAG="iAMMainActivity"
+    lateinit var btnretreive: Button
+    lateinit var btnsave: Button
+    lateinit var tv: TextView
+    lateinit var edname: EditText
+    lateinit var edloc: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edname = findViewById(R.id.edName)
+        edloc = findViewById(R.id.edLocation)
+        btnretreive = findViewById(R.id.button)
+        btnsave = findViewById(R.id.button2)
+        tv = findViewById(R.id.textView)
 
         //step 3:Initialize cloud firestore
         val db = Firebase.firestore
@@ -19,20 +35,45 @@ class MainActivity : AppCompatActivity() {
         // Create a new user with a first and last name
         //when i run this code, it is suppose to save these data to firebase file store
         //(but i have to create database for the first time before run the code)
-        val user = hashMapOf(
-            "first" to "Ada",
-            "last" to "Lovelace",
-            "born" to 1815
-        )
+        btnsave.setOnClickListener(){
+            if (edname.text.isNotEmpty()&&edloc.text.isNotEmpty()){
+                val user = hashMapOf(
+                    "name" to edname.text.toString(),
+                    "location" to edloc.text.toString()
+                )
+                // Add a new document with a generated ID
+                db.collection("users").add(user)
+                    .addOnSuccessListener { documentReference ->
+                        Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w(TAG, "Error adding document", e)
+                    }
 
-// Add a new document with a generated ID
-        db.collection("users")
-            .add(user)
-            .addOnSuccessListener { documentReference ->
-                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-            }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "Error adding document", e)
-            }
-    }
+            }else
+                Toast.makeText(applicationContext, "please fill missing entries", Toast.LENGTH_SHORT).show()
+        }
+
+
+        //step 5: Retrive data
+        btnretreive.setOnClickListener(){
+            db.collection("users").get()
+                .addOnSuccessListener{ result ->
+                    var details = "\n"
+                    for ( i in result) {
+                        //since we enter the data with hashMapOf we will get them with map
+                        i.data.map { (k,v)->
+                            details = details + "$k = $v \n\n" }
+                    }
+                    tv.text=" $details"
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "Error getting documents.", exception)
+                }
+        }
+
+
+    }//end on create()
+
+
 }
